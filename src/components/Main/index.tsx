6a50fb76f2420bf23b87b2181c43b03e66bfd2f6
Anderson Araujo <anderson.dos.santos.araujo@hotmail.com>
2023-10-29 04:17:02 -0300

    feat: added styles and test (#18)

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,8 +1,21 @@
+import * as S from './styles'
+
 const Main = () => {
   return (
-    <main>
-      <h1>React Avançado</h1>
-    </main>
+    <S.Wrapper>
+      <S.Logo
+        src="/img/logo.svg"
+        alt="Imagem de um átomo e Ract Avançado escrito ao lado."
+      />
+      <S.Title>React Avançado</S.Title>
+      <S.Description>
+        TypeScript, ReactJS, NextJS e Styled Components
+      </S.Description>
+      <S.Illustration
+        src="/img/hero-illustration.svg"
+        alt="Um desenvolvedor em frente a uma tela com código."
+      />
+    </S.Wrapper>
   )
 }
 

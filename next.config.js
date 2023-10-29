fc9b6660caee4ae3ca0ce9ba759bf2416ee1550c
Anderson Araujo <anderson.dos.santos.araujo@hotmail.com>
2023-10-29 04:45:29 -0300

    feat: pwa config (#20)

---
+++ next.config.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require('next-pwa')
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = withPWA({
+  dest: 'public',
+  disable: !isProd
+})

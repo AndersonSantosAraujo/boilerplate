074595d101f1694b0b96a67b9531577d38d9c7d7
Anderson Araujo <anderson.dos.santos.araujo@hotmail.com>
2023-10-29 01:10:43 -0300

    feat: config prettier with eslint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import styles from '../../styles/Home.module.css'
 
-export default function Home({title = "Next.js!"}: {title: string}) {
+export default function Home({ title = 'Next.js!' }: { title: string }) {
   return (
     <div className={styles.container}>
       <Head>

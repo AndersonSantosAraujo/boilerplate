d1e80599fcae60de0d859016e9bc555369345b3d
Anderson Araujo <anderson.dos.santos.araujo@hotmail.com>
2023-10-29 04:00:02 -0300

    feat: global styled components (#17)

--- src/pages/_documents.tsx
+++ src/pages/_documents.tsx
@@ -8,7 +8,7 @@ import Document, {
 } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
-export default class MyDocument extends Document {
+class MyDocument extends Document {
   static async getInitialProps(
     ctx: DocumentContext
   ): Promise<DocumentInitialProps> {
@@ -49,3 +49,5 @@ export default class MyDocument extends Document {
     )
   }
 }
+
+export default MyDocument
